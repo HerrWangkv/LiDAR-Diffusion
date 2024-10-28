5edce0aef01e948dc0c4c33933c7b61ebf6d8930
Kaiwen Wang <kaiwen.wang@kit.edu>
2024-10-28 16:35:17 +0100

    new env preparation

--- init/create_env.sh
+++ init/create_env.sh
@@ -10,15 +10,21 @@ conda activate lidar_diffusion
 
 # install dependencies
 pip install --upgrade pip
-pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu118
 pip install torchmetrics==0.5.0 pytorch-lightning==1.4.2 omegaconf==2.1.1 einops==0.3.0 transformers==4.36.2 imageio==2.9.0 imageio-ffmpeg==0.4.2 opencv-python kornia==0.7.0 wandb more_itertools
-pip install gdown
+pip install gdown scipy pyyaml joblib
 pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
 pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
-pip install joblib
+
+# install Google Sparse Hash library
+cd ..
+git clone https://github.com/sparsehash/sparsehash.git
+cd sparsehash
+./configure --prefix=/home/kwang/opt
+make
+make install
 
 # install torchsparse (optional)
-#apt-get install libsparsehash-dev
-#pip install git+https://github.com/mit-han-lab/torchsparse.git@v1.4.0
-
-mkdir -p dataset/
+export PATH=/usr/local/cuda-11.8/bin:$PATH
+export CPLUS_INCLUDE_PATH=/home/kwang/opt/include
+pip install git+https://github.com/mit-han-lab/torchsparse.git@v1.4.0
